4d96dbc9bdb7640e8c9a0758410f86b417a72217
unknown <ssoon1950@gmail.com>
2024-06-12 17:38:33 +0900

    'CRUD'

--- src/main/java/com/pss/sar/SarApplication.java
+++ src/main/java/com/pss/sar/SarApplication.java
@@ -1,14 +1,27 @@
 package com.pss.sar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pss.sar.domain.Car;
+import com.pss.sar.domain.CarRepository;
+
 @SpringBootApplication
 @RestController
-public class SarApplication {
+public class SarApplication implements CommandLineRunner{
+
+	private static final Logger logger = 
+		LoggerFactory.getLogger(SarApplication.class);
+
+	@Autowired
+	private CarRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SarApplication.class, args);
@@ -17,5 +30,18 @@ public class SarApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value="name", defaultValue="World") String name){
 		return String.format("Hello %s!", name);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2021, 59000));
+		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000));
+		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000));
+
+		for (Car car : repository.findAll()) {
+			logger.info(car.getBrand() + " " + car.getModel());
+		}
 	} 
+
+	
 }

8a19b8934be3b7ba9e7e52a501a6e85bab67b594
Sangsoon Park <ssoon1950@gmail.com>
2024-06-12 21:13:20 +0900

    CRUD

--- src/main/java/com/pss/sar/domain/CarRepository.java
+++ src/main/java/com/pss/sar/domain/CarRepository.java
@@ -2,6 +2,16 @@ package com.pss.sar.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CarRepository extends CrudRepository<Car, Long>{
 
+    //브랜드로 자동차를 검색
+    List<Car> findByBrand(String brand);
+
+    //색상으로 자동차를 검색
+    List<Car> findByColor(String Color);
+
+    //연도로 자동차를 검색
+    List<Car> findByYear(int year);
 }

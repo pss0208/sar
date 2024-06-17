4c2cff51c741a1fd33c2b4007586b9ea72954147
unknown <ssoon1950@gmail.com>
2024-06-17 17:45:18 +0900

    complete postman

--- src/main/java/com/pss/sar/domain/CarRepository.java
+++ src/main/java/com/pss/sar/domain/CarRepository.java
@@ -1,17 +1,23 @@
 package com.pss.sar.domain;
 
-import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long>{
 
     //브랜드로 자동차를 검색
-    List<Car> findByBrand(String brand);
+    // List<Car> findByBrand(String brand);
+    List<Car> findByBrand(@Param("brand") String brand);
 
     //색상으로 자동차를 검색
-    List<Car> findByColor(String Color);
+    // List<Car> findByColor(String Color);
+    List<Car> findByColor(@Param("color") String color);
 
     //연도로 자동차를 검색
-    List<Car> findByYear(int year);
+    // List<Car> findByYear(int year);
+    List<Car> findByYear(@Param("year") int year);
 }

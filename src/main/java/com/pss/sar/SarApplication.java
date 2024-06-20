e26e3cba067b8d5b113ce860d1f1c60c1f343842
unknown <ssoon1950@gmail.com>
2024-06-20 17:25:13 +0900

    Spring Security

--- src/main/java/com/pss/sar/SarApplication.java
+++ src/main/java/com/pss/sar/SarApplication.java
@@ -66,7 +66,7 @@ public class SarApplication implements CommandLineRunner{
 		userRepository.save(new User("user",
 		"$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue", "USER"));
 		userRepository.save(new User("admin",
-		"$2a$10$8cjz47bjbR4Mn8Mg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW", "ADMIN"));
+		"$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW", "ADMIN"));
 	} 
 
 }
